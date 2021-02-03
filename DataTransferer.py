54bd8bbf593ff355ee4093c62ec0ba5ecaef4468
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-02-03 23:57:00 +0000

    Forgot to commit data transferer

---
+++ DataTransferer.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+try:
+    import cupy
+except ModuleNotFoundError:
+    pass
+
+class DataTransferer:
+    def __init__(self, xp):
+        if xp.__name__ == "numpy":
+            self.to_host = self.__no_transfer
+            self.from_host = self.__no_transfer
+        elif xp.__name__ == "cupy":
+            self.to_host = self.__cupy_to_numpy
+            self.from_host = self.__numpy_to_cupy
+
+    def __no_transfer(self, data):
+        return data
+
+    def __numpy_to_cupy(self, data):
+        return cupy.asarray(data)
+
+    def __cupy_to_numpy(self, data):
+        return cupy.asnumpy(data)
