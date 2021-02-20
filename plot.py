0982fb014771375fe0b32e9856b0753cd8b476ff
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-02-20 17:35:19 +0000

    Plotting scripts are easier to use

--- plot.py
+++ plot.py
@@ -1,17 +1,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import time
-import sys
+import argparse
 
-npy_fname = sys.argv[1]
+parser = argparse.ArgumentParser(description='Plot Melvin save files')
+parser.add_argument('filename', help='file to plot')
+parser.add_argument('--aspect_ratio', default=2,  help='aspect ratio of physical data')
+parser.add_argument('--contour', action='store_true', help='enables contour plotting')
+parser.add_argument('--output', help='output file')
+parser.add_argument('--slice', nargs=2, type=int, help='axis to slice along')
+args = parser.parse_args()
 
-extent = [0, 335, 0 ,536]
+filename = args.filename
 
-png_fname = npy_fname[:-3] + "png"
-start = time.time()
-data = np.load(npy_fname)
-vmax = max(abs(data.min()), data.max())
-plt.imshow(data.T, origin='lower', extent=extent, cmap='RdBu', vmax=vmax, vmin=-vmax)
-plt.savefig(png_fname)
-diff = time.time() - start
-print(diff)
+data = np.load(filename)
+if data.dtype == 'complex128':
+    mode = 'spectral'
+    data = np.absolute(data)
+else:
+    mode = 'physical'
+
+if args.slice:
+    slice_axis = args.slice[0]
+    slice_idx = args.slice[1]
+    data_slice = data.take(slice_idx, axis=slice_axis)
+    plt.plot(data_slice)
+else:
+    if mode == 'spectral':
+        img = plt.imshow(data.T, origin='lower')
+        plt.colorbar(img)
+    elif mode == 'physical':
+        extent = [0, 1, 0, args.aspect_ratio]
+        if args.contour:
+            plt.contour(data.T, origin='lower', extent=extent)
+        else:
+            img = plt.imshow(data.T, origin='lower', extent=extent)
+            plt.colorbar(img)
+
+plt.axis('off')
+plt.tight_layout()
+
+if args.output:
+    plt.savefig(args.output)
+else:
+    plt.show()
---
+++ plot_dump.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+
+parser = argparse.ArgumentParser(description='Plot Melvin dump files')
+parser.add_argument('filename', help='file to plot')
+parser.add_argument('--var', required=True, help='variable to plot')
+parser.add_argument('--output', help='output file')
+parser.add_argument('--slice', nargs=2, type=int, help='axis to slice along')
+args = parser.parse_args()
+
+filename = args.filename
+var = args.var
+
+npzdata = np.load(filename)
+data = np.absolute(npzdata[var])
+
+if args.slice:
+    slice_axis = args.slice[0]
+    slice_idx = args.slice[1]
+    data_slice = data.take(slice_idx, axis=slice_axis)
+    plt.plot(data_slice)
+else:
+    img = plt.imshow(data.T, origin='lower')
+    plt.colorbar(img)
+
+if args.output:
+    plt.savefig(args.output)
+else:
+    plt.show()
