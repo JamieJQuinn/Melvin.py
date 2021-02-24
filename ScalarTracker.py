0164c2d9c22bc65c057c2c9bfe484c0d349d7974
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-02-24 20:16:59 +0000

    Didn't realise defining variables in class body meant they're static... DOH

--- ScalarTracker.py
+++ ScalarTracker.py
@@ -1,14 +1,14 @@
 import numpy as np
 
 class ScalarTracker:
-    """Tracks energy through time"""
-    _values = []
-    _times = []
+    """Tracks a scalar through time"""
 
     def __init__(self, params, xp, filename):
         self._xp = xp
         self._params = params
         self._filename = filename
+        self._values = []
+        self._times = []
 
     def append(self, t, val):
         self._times += [t]
@@ -16,4 +16,12 @@ class ScalarTracker:
 
     def save(self):
         """Saves to file"""
-        np.save(self._filename, np.array((self._times, self._values)))
+        t = self._xp.array(self._times)
+        vals = self._xp.array(self._values)
+        self._xp.savez(self._filename, t=t, values=vals)
+
+    # def load(self):
+        # """Loads from file"""
+        # data = self._xp.load(self._filename)
+        # self._times = data['t'].to_arr()
+        # self._values = data['values'].to_arr()
