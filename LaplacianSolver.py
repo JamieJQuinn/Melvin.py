98baa7270b8a0081c4a699ba7905800c07464b5d
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-02-24 20:15:42 +0000

    Kind of bugfix in LaplacianSolver. It was assuming the form $\nabla^2 x = -y$

--- LaplacianSolver.py
+++ LaplacianSolver.py
@@ -7,10 +7,9 @@ class LaplacianSolver:
 
         # Laplacian matrix
         self.lap = -((n*p.kn)**2 + (m*p.km)**2)
-        self.lap[0,0] = 1
 
     def solve(self, in_arr, out_arr):
         # Avoid div by 0 errors
         self.lap[0,0] = 1.0
-        out_arr[:] = -in_arr/self.lap
+        out_arr[:] = in_arr/self.lap
         self.lap[0,0] = 0.0
