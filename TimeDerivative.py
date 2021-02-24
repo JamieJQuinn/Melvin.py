db620fe2e090279b624b1011683c1d9c63a3c5f3
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-02-24 20:18:04 +0000

    TimeDerivative can now handle loading from array

--- TimeDerivative.py
+++ TimeDerivative.py
@@ -1,9 +1,12 @@
+from Variable import scale_variable
+
 class TimeDerivative:
     """
     Represents a sequence of derivatives in time
     """
     def __init__(self, params, xp):
         self._params = params
+        self._xp = xp
         self._curr_idx = 0
         self._data = xp.zeros((params.integrator_order, 2*params.nn+1, params.nm), dtype=params.complex)
 
@@ -25,5 +28,26 @@ class TimeDerivative:
     def get(self, idx=0):
         return self._data[self._curr_idx+idx]
 
+    def get_all(self):
+        return self._data
+
     def set(self, data, idx=0):
         self._data[self._curr_idx+idx] = data
+
+    def load(self, data):
+        if isinstance(data, str):
+            self.__load_from_file(data)
+        else:
+            self.__load_from_array(data)
+
+    def __load_from_file(self, data):
+        raise NotImplementedError
+
+    def __load_from_array(self, data):
+        # Assume data already on host
+        nn, nm = self._params.nn, self._params.nm
+        print(data.shape)
+        for i in range(self._params.integrator_order):
+            if data[i].shape != (nn, nm):
+                data[i] = scale_variable(data[i], (nn, nm), self._xp)
+            self._data[i] = data[i]
