cbc6bb9288e2843882243f7e1dcc64cbccc4272c
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-04-30 10:37:39 +0100

    Add half-spectral array creation in ArrayFactory

--- ArrayFactory.py
+++ ArrayFactory.py
@@ -4,6 +4,11 @@ class ArrayFactory:
         self._p = params
         self._xp = xp
 
+        if params.is_fully_spectral():
+            self.make_spectral = self._make_all_spectral
+        else:
+            self.make_spectral = self._make_semi_spectral
+
     def make_mode_number_matrices(self):
         params = self._p
 
@@ -27,6 +32,27 @@ class ArrayFactory:
         dtype = self._p.complex
 
         return self._xp.zeros((nn, nm), dtype=dtype)
+
+    def _make_semi_spectral(self, ni=None, nj=None):
+        """Create array representing spectral data
+        :param ni: Number of elements in x direction,
+        defaults to value specified in parameters
+        :type ni: int, optional
+        :param nj: Number of elements in z direction,
+        defaults to value specified in parameters
+        :type nj: int, optional
+        """
+
+        # TODO refactor this and _make_all_spectral
+
+        if ni is None:
+            ni = self._p.nn
+        if nj is None:
+            nj = self._p.nz
+        dtype = self._p.complex
+
+        return self._xp.zeros((ni, nj), dtype=dtype)
+
 
     def make_physical(self, nx=None, nz=None):
         """Create array representing physical data
---
+++ ArrayFactory_test.py
@@ -0,0 +1,21 @@
+import pytest
+import numpy as np
+
+from ArrayFactory import ArrayFactory
+
+def test_fully_spectral_array_production(parameters):
+    array_factory = ArrayFactory(parameters, np)
+
+    spectral = array_factory.make_spectral()
+    physical = array_factory.make_physical()
+
+    assert spectral.shape == (2*parameters.nn +1, parameters.nm)
+
+def test_fdm_array_production(fdm_parameters):
+    array_factory = ArrayFactory(fdm_parameters, np)
+
+    spectral = array_factory.make_spectral()
+    physical = array_factory.make_physical()
+
+    assert spectral.shape == (fdm_parameters.nn, fdm_parameters.nz)
+    assert physical.shape == (fdm_parameters.nx, fdm_parameters.nz)
