1a2240c085981a7d31a53a6072c860635ed27134
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-04-30 10:36:57 +0100

    Add utility functions for loading specific sparse matrix solvers

--- utility.py
+++ utility.py
@@ -1,4 +1,6 @@
 import numpy as np
+import scipy.sparse
+import scipy.sparse.linalg
 
 def sech(x):
     return 1.0/np.cosh(x)
@@ -8,3 +10,17 @@ def set_numpy_module(PARAMS):
         return cupy
     else:
         return numpy
+
+def load_scipy_sparse(xp):
+    if xp.__name__ == "numpy":
+        return scipy.sparse
+    elif xp.__name__ == "cupy":
+        import cupyx
+        return cupyx.scipy.sparse
+
+def load_scipy_sparse_linalg(xp):
+    if xp.__name__ == "numpy":
+        return scipy.sparse.linalg
+    elif xp.__name__ == "cupy":
+        import cupyx.scipy.sparse.linalg
+        return cupyx.scipy.sparse.linalg
