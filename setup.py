fb40feab56dde90f37971cfa159a620a30610fd5
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-05-03 19:44:40 +0100

    Add pyproject.toml and setup.cfg

--- setup.py
+++ setup.py
@@ -1,14 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='melvin',
-    version='0.1',
-    license='MIT',
-    author='Jamie Quinn',
-    author_email='jamiejquinn@jamiejquinn.com',
-    packages=['melvin'],
-    url='https://github.com/jamiejquinn/melvin.py',
-    extras_require={
-        'cupy': ['cupy']
-    }
-)
+setup()
