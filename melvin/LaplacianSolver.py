e9b21c0a8b5081b1bc55c7e1c4f4c4206f6f1505
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2022-02-22 20:32:05 +0000

    Ensure laplacian is complex valued

--- melvin/LaplacianSolver.py
+++ melvin/LaplacianSolver.py
@@ -39,7 +39,7 @@ class LaplacianSolver:
                         offset,
                     ),
                     shape=(p.nz, p.nz),
-                    dtype=p.float,
+                    dtype=p.complex,
                 ).tocsr()
                 for n_ in range(p.nn)
             ]
